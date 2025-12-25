71237f3ce600b80ebebd7d05cc2bc2ca7f6b5aa7
Tom Hoskins <tomhoskins0712@gmail.com>
2025-12-24 23:28:43 -0600

    Initial commit, connected to gemini

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+from google import genai
+from dotenv import load_dotenv
+
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    load_dotenv()
+    #prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("gemini api key not found")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=args.user_prompt
+        )
+    
+    if response.usage_metadata == None:
+        raise RuntimeError("API call error")
+    print(f"Prompt:\n{args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
+
+if __name__ == "__main__":
+    main()
