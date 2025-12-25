1074fb9206097b467617ae7af98f3856202fa579
Tom Hoskins <tomhoskins0712@gmail.com>
2025-12-24 23:52:03 -0600

    Added optional verbose parameter to clean up terminal interface

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import argparse
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
     load_dotenv()
@@ -15,16 +17,18 @@ def main():
         raise RuntimeError("gemini api key not found")
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=args.user_prompt
+        contents=messages
         )
     
     if response.usage_metadata == None:
         raise RuntimeError("API call error")
-    print(f"Prompt:\n{args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt:\n{messages}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
