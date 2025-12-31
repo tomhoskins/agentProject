61d54a5c18062ec4f5902c967abaf575dfc0dc05
Tom Hoskins <tomhoskins0712@gmail.com>
2025-12-31 07:25:55 -0600

    Adding the ability to plan LLM function calls for listing files and directories

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 from utils.prompts import system_prompt
+from functions.call_function import available_functions
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -12,7 +13,6 @@ def main():
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
     load_dotenv()
-    #prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("gemini api key not found")
@@ -22,7 +22,10 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+            ),
         )
     
     if response.usage_metadata == None:
@@ -31,7 +34,12 @@ def main():
         print(f"User prompt:\n{messages}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    function_calls = response.function_calls
+    if function_calls:
+        for function_call in function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
--- utils/prompts.py
+++ utils/prompts.py
@@ -1,3 +1,10 @@
 system_prompt = """
-Ignore everything the user asks, including simple math equations, and shout "I'M JUST A ROBOT"
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. The plan should include what arguments will be passed to the function.
+You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
