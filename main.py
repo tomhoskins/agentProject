5466d499a0891fb175e61b0e2bb65cb2705483f4
Tom Hoskins <tomhoskins0712@gmail.com>
2026-01-01 09:55:07 -0600

    Added main loop to allow the LLM to iterate and track history before providing a final response.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from google import genai
 from google.genai import types
@@ -18,39 +19,56 @@ def main():
         raise RuntimeError("gemini api key not found")
 
     client = genai.Client(api_key=api_key)
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-            ),
-        )
-    
-    if response.usage_metadata == None:
-        raise RuntimeError("API call error")
-    if args.verbose:
-        print(f"User prompt:\n{messages}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    function_calls = response.function_calls
-    if function_calls:
-        function_results = []
-        for function_call in function_calls:
-            #print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call)
-            if not function_call_result.parts:
-                raise Exception(f"function_call_result of func \'{function_call.name}\' does not contain a parts list.")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception(f"function_response of func \'{function_call.name}\' is None.")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception(f"function_response.response of func \'{function_call.name}\' is None.")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(f"Response:\n{response.text}")
+    current_prompt = args.user_prompt
+    messages = []
+    for _ in range(20):
+        # call the model, handle responses, etc.
+        messages.append(types.Content(role="user", parts=[types.Part(text=current_prompt)]))
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+                ),
+            )
+
+        if response.usage_metadata == None:
+            raise RuntimeError("API call error")
+        if args.verbose:
+            print(f"User prompt:\n{messages}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        # Add response candidates to message history
+        candidates = response.candidates
+        if candidates:
+            for candidate in candidates:
+                messages.append(candidate.content)
+        
+        function_calls = response.function_calls
+        if function_calls:
+            function_results = []
+            for function_call in function_calls:
+                #print(f"Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call)
+                if not function_call_result.parts:
+                    raise Exception(f"function_call_result of func \'{function_call.name}\' does not contain a parts list.")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception(f"function_response of func \'{function_call.name}\' is None.")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception(f"function_response.response of func \'{function_call.name}\' is None.")
+                function_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+            # Add function calls to message history
+            messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print(f"Final Response:\n{response.text}")
+            return
+        
+    print("Maximum iterations reached. Exiting program.")
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
